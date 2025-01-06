cf4d90e73a90ed39fa401278523f6eea71a3c03f
Siddhant Yadav <siddthecoder@gmail.com>
2025-01-06 11:33:12 +0545

    Hey

--- Counter_useState/src/App.jsx
+++ Counter_useState/src/App.jsx
@@ -26,10 +26,17 @@ function App() {
   }
    return(
     <>
+    <p>Lorem ipsum dolor sit amet.{count} <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, facilis. Numquam cum perspiciatis id minus labore deleniti aperiam quis inventore.{count} <br />
+    Lorem ipsum dolor sit amet.{count} <br />
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet odit doloremque, optio a nam at dolor nisi aliquam placeat quam.{count}
+    </p>
      <button>{count}</button>
      <br />
      <button onClick={increment}>Increment</button>
      <button onClick={decrement}>Decrement</button>
+
+     <h3>SO in this way we can change the state through useState hook in React example : {count}</h3>
     </>
    )
 }
